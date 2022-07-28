10179c6466a4719bcdc0b6d1732f6f236a181620
Ilya Zherebtsov <izherebcov@gmail.com>
2022-07-28 15:23:50 +0400

    создание формы

--- index.php
+++ index.php
@@ -5,10 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Список дел</title>
-    <link rel="stylesheet" href="css/tsyle.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
 </head>
 <body>
-    
+    <div class="container">
+        <h1>Списописк дел</h1>
+        <form action="/add.php" method="POST">
+            <input type="text" name="task" id="task" placeholder="Нужно сделать" class="form-control">
+            <button type="submit" name="sendTask" class="btn btn-success">Отправить</button>
+        </form>
+
+    </div>
 </body>
 </html>
